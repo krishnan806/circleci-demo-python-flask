da052a474124cb2c4534e7c8dba033b2afbb7bd1
Muralikrishnan <krishnan806@gmail.com>
2020-05-27 21:42:09 -0400

    Updates for 2020-05-27

---
+++ tests/test_math.py
@@ -0,0 +1,12 @@
+import pytest
+# from handler.math_handler import add
+def add (params):
+    result = int(params['a']) + int(params['b'])
+    print("finished")
+    return result
+def test_math():
+    params = {
+        'a' : 1,
+        'b' : 2
+    }
+    assert add(params) == 3
